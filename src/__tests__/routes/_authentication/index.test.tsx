f167a8d939cd122cec4cc4d73a43f6bfaebe79f2
Mouadh HSOUMI <hsmouadh@gmail.com>
2024-08-22 01:07:45 +0200

    test: fix and adjust tests

--- src/__tests__/routes/_authentication/index.test.tsx
+++ src/__tests__/routes/_authentication/index.test.tsx
@@ -1,12 +1,19 @@
-import { screen, waitFor } from "@testing-library/react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { AuthenticationContext } from "../../../contexts/authentication";
-import { MemeFeedPage } from "../../../routes/_authentication/index";
-import { renderWithRouter } from "../../utils";
+import 'react-intersection-observer/test-utils';
 
-describe("routes/_authentication/index", () => {
-  describe("MemeFeedPage", () => {
+import { screen, waitFor } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { AuthenticationContext } from '../../../contexts/authentication';
+import { MemeFeedPage } from '../../../routes/_authentication/index';
+import { renderWithRouter } from '../../utils';
+
+// We mock the LazyLoadImage component to avoid rendering the actual image
+vi.mock('react-lazy-load-image-component', () => ({
+  LazyLoadImage: vi.fn(props => <img {...props} />),
+}));
+
+describe('routes/_authentication/index', () => {
+  describe('MemeFeedPage', () => {
     function renderMemeFeedPage() {
       return renderWithRouter({
         component: MemeFeedPage,
@@ -17,8 +24,8 @@ describe("routes/_authentication/index", () => {
                 value={{
                   state: {
                     isAuthenticated: true,
-                    userId: "dummy_user_id",
-                    token: "dummy_token",
+                    userId: 'dummy_user_id',
+                    token: 'dummy_token',
                   },
                   authenticate: () => {},
                   signout: () => {},
@@ -32,47 +39,60 @@ describe("routes/_authentication/index", () => {
       });
     }
 
-    it("should fetch the memes and display them with their comments", async () => {
+    it('should fetch the memes and display them with their comments', async () => {
       renderMemeFeedPage();
 
       await waitFor(() => {
         // We check that the right author's username is displayed
-        expect(screen.getByTestId("meme-author-dummy_meme_id_1")).toHaveTextContent('dummy_user_1');
-        
+        expect(screen.getByTestId('meme-author-dummy_meme_id_1')).toHaveTextContent('dummy_user_1');
+
         // We check that the right meme's picture is displayed
-        expect(screen.getByTestId("meme-picture-dummy_meme_id_1")).toHaveStyle({
-          'background-image': 'url("https://dummy.url/meme/1")',
-        });
+        expect(screen.getByTestId('meme-picture-dummy_meme_id_1')).toHaveAttribute('src', 'https://dummy.url/meme/1');
 
         // We check that the right texts are displayed at the right positions
-        const text1 = screen.getByTestId("meme-picture-dummy_meme_id_1-text-0");
-        const text2 = screen.getByTestId("meme-picture-dummy_meme_id_1-text-1");
+        const text1 = screen.getByTestId('meme-picture-dummy_meme_id_1-text-0');
+        const text2 = screen.getByTestId('meme-picture-dummy_meme_id_1-text-1');
         expect(text1).toHaveTextContent('dummy text 1');
         expect(text1).toHaveStyle({
-          'top': '0px',
-          'left': '0px',
+          top: '0px',
+          left: '0px',
         });
         expect(text2).toHaveTextContent('dummy text 2');
         expect(text2).toHaveStyle({
-          'top': '100px',
-          'left': '100px',
+          top: '100px',
+          left: '100px',
         });
 
         // We check that the right description is displayed
-        expect(screen.getByTestId("meme-description-dummy_meme_id_1")).toHaveTextContent('dummy meme 1');
-        
-        // We check that the right number of comments is displayed
-        expect(screen.getByTestId("meme-comments-count-dummy_meme_id_1")).toHaveTextContent('3 comments');
-        
-        // We check that the right comments with the right authors are displayed
-        expect(screen.getByTestId("meme-comment-content-dummy_meme_id_1-dummy_comment_id_1")).toHaveTextContent('dummy comment 1');
-        expect(screen.getByTestId("meme-comment-author-dummy_meme_id_1-dummy_comment_id_1")).toHaveTextContent('dummy_user_1');
+        expect(screen.getByTestId('meme-description-dummy_meme_id_1')).toHaveTextContent('dummy meme 1');
 
-        expect(screen.getByTestId("meme-comment-content-dummy_meme_id_1-dummy_comment_id_2")).toHaveTextContent('dummy comment 2');
-        expect(screen.getByTestId("meme-comment-author-dummy_meme_id_1-dummy_comment_id_2")).toHaveTextContent('dummy_user_2');
-        
-        expect(screen.getByTestId("meme-comment-content-dummy_meme_id_1-dummy_comment_id_3")).toHaveTextContent('dummy comment 3');
-        expect(screen.getByTestId("meme-comment-author-dummy_meme_id_1-dummy_comment_id_3")).toHaveTextContent('dummy_user_3');
+        // We check that the right number of comments is displayed
+        expect(screen.getByTestId('meme-comments-count-dummy_meme_id_1')).toHaveTextContent('3 comments');
+
+        // We click on the comments section
+        screen.getByTestId('meme-comments-section-dummy_meme_id_1').click();
+
+        // We check that the right comments with the right authors are displayed
+        expect(screen.getByTestId('meme-comment-content-dummy_meme_id_1-dummy_comment_id_1')).toHaveTextContent(
+          'dummy comment 1',
+        );
+        expect(screen.getByTestId('meme-comment-author-dummy_meme_id_1-dummy_comment_id_1')).toHaveTextContent(
+          'dummy_user_1',
+        );
+
+        expect(screen.getByTestId('meme-comment-content-dummy_meme_id_1-dummy_comment_id_2')).toHaveTextContent(
+          'dummy comment 2',
+        );
+        expect(screen.getByTestId('meme-comment-author-dummy_meme_id_1-dummy_comment_id_2')).toHaveTextContent(
+          'dummy_user_2',
+        );
+
+        expect(screen.getByTestId('meme-comment-content-dummy_meme_id_1-dummy_comment_id_3')).toHaveTextContent(
+          'dummy comment 3',
+        );
+        expect(screen.getByTestId('meme-comment-author-dummy_meme_id_1-dummy_comment_id_3')).toHaveTextContent(
+          'dummy_user_3',
+        );
       });
     });
   });
